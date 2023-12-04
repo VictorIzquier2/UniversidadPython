822932518a710838c8cf793f703b02c5ed82d137
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2023-12-04 22:35:54 +0100

    Ejercicio Imprimir contacto

---
+++ 001 - Introduccion/002 - Ejercicio.py
@@ -0,0 +1,11 @@
+# Declaración de Variables
+
+def imprimirContacto(nombreCompleto, telefonoMovil, correoElectronico):
+  print(f"Nombre: {nombreCompleto}\nTeléfono: {telefonoMovil}\nCorreo: {correoElectronico}\n")
+  
+nombre = "Víctor Izquierdo"
+telefono = 662086316
+correo = "victor.izquierdo1985@gmail.com"
+
+if __name__ == '__main__':
+  imprimirContacto(nombre, telefono, correo)
