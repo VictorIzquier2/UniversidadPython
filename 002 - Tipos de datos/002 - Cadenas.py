2f66d87ff0c08dc0c84521d6f3276d9ce28e9f7f
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2023-12-04 23:40:47 +0100

    Cadenas de texto en Python y concatenacion

---
+++ 002 - Tipos de datos/002 - Cadenas.py
@@ -0,0 +1,8 @@
+miGrupoFavorito = "Metallica"
+comentario = "la mejor banda de rock"
+print("Mi grupo favorito es: " + miGrupoFavorito + " " + comentario)
+
+numero1 = "1"
+numero2 = "2"
+print("Concatenación: ", numero1 + numero2)
+print("Conversión: ", int(numero1) + int(numero2))
