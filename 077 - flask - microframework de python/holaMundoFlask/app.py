c309e36950be708b097b09d074a6f4162460472b
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2024-01-08 23:59:18 +0100

    Manejo de sesiones en flask

--- 077 - flask - microframework de python/holaMundoFlask/app.py
+++ 077 - flask - microframework de python/holaMundoFlask/app.py
@@ -1,16 +1,38 @@
-from flask import Flask, request, render_template, url_for, abort
+from flask import Flask, request, render_template, url_for, abort, jsonify, session
 from werkzeug.utils import redirect
 from logger_base import log  # Importa la función de configuración
 
 app = Flask(__name__)
 
+app.secret_key = 'Mi_llave_Secreta'
+
 log(app, 'DEBUG') # Configurar el nivel de logger para esta app
+
+###############################################################
+# http://localhost:5000/
 
 @app.route('/')
 def inicio():
-    app.logger.info(f'Entramos al path {request.path}')   
+    if 'username' in session:
+      return f'Bienvenido {session["username"]}'
+    else:
+      return 'No ha hecho login'
+    #app.logger.info(f'Entramos al path {request.path}')   
+    #return 'Hola mundo desde Flask.'
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    # Omitimos la validación de usuario y password
+    # Agregar el usuario a la sesión
+    session['username'] = request.form['username']
+    return redirect(url_for('inicio'))
+  return render_template('login.html')
 
-    return 'Hola mundo desde Flask.'
+@app.route('/logout')
+def logout():
+  session.pop('username')
+  return redirect(url_for('inicio'))
   
 @app.route('/saludar/<nombre>')
 def saludar(nombre):
@@ -41,13 +63,15 @@ def pagina_no_encontrada(error):
   return (render_template('error404.html', error=error), 404)
 
 ###############################################################
-
 # REST Representational state transfer
+
 @app.route('/api/mostrar/<nombre>', methods=['GET', 'POST'])
 def mostrar_json(nombre):
   valores = {'nombre': nombre, 'metodo_http': request.method}
   
   return valores
+
+# PRUEBAS
 
 if __name__=='__main__':
     app.run(debug=True)
